d88a1e7932e5bd578b6dcc5daa07c70c8226d371
Mehul Jani <mjani@deloitte.com.au>
2021-10-28 12:34:16 +0800

    Add mongoDB

--- server.js
+++ server.js
@@ -2,18 +2,21 @@ const express = require("express");
 const dotenv = require("dotenv");
 //const logger = require('./middleware/logger');
 const morgan = require("morgan");
+const connectDB = require('./config/db');
+
+dotenv.config({ path: "./config/config.env" });
+
+connectDB();
 
 //Route files
 const bootcamps = require('./routes/bootcamps');
 
-dotenv.config({ path: ".config/config/env" });
-
 const app = express();
 
 // Dev logging middleware
-//if (process.env.NODE_ENV === 'development'){
+if (process.env.NODE_ENV === 'development'){
   app.use(morgan('dev'));
-//}
+}
 
 //app.use(logger);
 
@@ -21,9 +24,15 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-
+const server = app.listen(PORT, () => {
   console.log(
     `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
   );
+});
+
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server & exit process
+
+  server.close(() => process.exit(1));
 });
