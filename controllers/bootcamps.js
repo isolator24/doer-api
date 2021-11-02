1bac6a6bfbcc35f264c5d5626dfeeb23aed9bacb
Mehul Jani <mjani@deloitte.com.au>
2021-11-02 12:55:50 +0800

    Added search and pagination functionality

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,40 +1,145 @@
+const ErrorResponse = require("../utilities/errorResponse");
+const asyncHandler = require("../middleware/async");
+const Bootcamp = require("../models/Bootcamps");
+
 // @desc    Get all bootcamps
 // @route   Get /api/v1/bootcamps
 // @access  Public
-exports.getBootcamps = (req, res, next) => {
-  // test my git skills
-  
-    res.status(200).json({ success: true, msg: "Show all bootcamps", hello: req.hello });
-};
+exports.getBootcamps = asyncHandler(async (req, res, next) => {
+  let query;
+
+  // copy req.query
+  const reqQuery = { ...req.query };
+
+  // Fields to exclude
+  const removeFields = ['select', 'sort', 'page', 'limit'];
+
+  // Loop over removeFields and delete them from reqQuery
+  removeFields.forEach(param => delete reqQuery[param]);
+
+  // Create query string
+  let queryStr = JSON.stringify(req.query);
+
+  // Create operator ($gt, $gte, etc)
+  queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+
+  // Finding resource
+  query = Bootcamp.find(JSON.parse(queryStr));
+
+  // Select Fields
+  if (req.query.select){
+      const fields = req.query.select.split(',').join(' ');
+      query = query.select(fields);
+  }
+
+  // Sort
+  if(req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+  } else {
+      query = query.sort('-createdAt');
+  }
+
+  // Pagination
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 1;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await Bootcamp.countDocuments();
+
+  query = query.skip(startIndex).limit(limit);
+
+  // Executing query
+  const bootcamps = await query;
+
+  // Pagination result
+  const pagination = {};
+
+  if (endIndex < total){
+      pagination.next = {
+          page: page + 1
+      }
+  }
+
+  if (startIndex > 0) {
+      pagination.prev = {
+          page: page -1,
+          limit
+      }
+  }
+
+  res.status(200).json({
+    success: true,
+    count: bootcamps.length,
+    pagination,
+    data: bootcamps,
+  });
+});
 
 // @desc    Get single bootcamp
 // @route   Get /api/v1/bootcamps/:id
 // @access  Public
-exports.getBootcamp = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Get bootcamp ${req.params.id}` });
-};
-  
+exports.getBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.findById(req.params.id);
+
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    );
+  }
+
+  res.status(200).json({
+    success: true,
+    data: bootcamp,
+  });
+});
+
 // @desc    Create new bootcamp
 // @route   POST /api/v1/bootcamps/:id
 // @access  Public
-exports.createBootcamp = (req, res, next) => {
-    res.status(200).json({ success: true, msg: "Create new bootcamp" });
-};
+exports.createBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.create(req.body);
+  res.status(201).json({
+    success: true,
+    data: bootcamp,
+  });
+});
 
 // @desc    Update bootcamp
 // @route   PUT /api/v1/bootcamps/:id
 // @access  Public
-exports.updateBootcamp = (req, res, next) => {
-    res
-    .status(200)
-    .json({ success: true, msg: `Update bootcamp ${req.params.id}` });};
+exports.updateBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    );
+  }
+
+  res.status(200).json({
+    success: true,
+    data: bootcamp,
+  });
+});
 
 // @desc    Delete bootcamp
 // @route   DELETE /api/v1/bootcamps/:id
 // @access  Public
-exports.deleteBootcamp = (req, res, next) => {
-    res
-    .status(200)
-    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
-};
-  
+exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    );
+  }
+
+  res.status(200).json({
+    success: true,
+    data: {},
+  });
+});
--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const dotenv = require("dotenv");
 //const logger = require('./middleware/logger');
 const morgan = require("morgan");
 const connectDB = require('./config/db');
-
+const colors = require('colors');
+const errorHandler = require('./middleware/error');
 dotenv.config({ path: "./config/config.env" });
 
 connectDB();
@@ -13,6 +14,9 @@ const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
+// Body parser
+app.use(express.json());
+
 // Dev logging middleware
 if (process.env.NODE_ENV === 'development'){
   app.use(morgan('dev'));
@@ -21,17 +25,17 @@ if (process.env.NODE_ENV === 'development'){
 //app.use(logger);
 
 app.use('/api/v1/bootcamps', bootcamps);
-
+app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () => {
   console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
   );
 });
 
 process.on('unhandledRejection', (err, promise) => {
-  console.log(`Error: ${err.message}`);
+  console.log(`Error: ${err.message}`.red.bold);
   // Close server & exit process
 
   server.close(() => process.exit(1));
